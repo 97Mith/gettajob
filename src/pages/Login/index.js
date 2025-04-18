64c1d84ba78e312ca6480428e1347c6fe4abc2d6
Matheus Tarasczuk <mathtarasczuk@gmail.com>
2025-04-18 16:29:36 -0300

    OK ~~13 - no Login, substituir a Label do título pela logo

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,24 +1,34 @@
-import React from "react";
-import { View, Text, TouchableOpacity, Image } from "react-native";
+import React, { useEffect, useRef } from "react";
+import { View, Text, TouchableOpacity, Image, Animated } from "react-native";
 import { useNavigation } from "@react-navigation/native";
 import InputField from "../../components/InputField";
 import SocialButton from "../../components/SocialButton";
 import styles from "../../styles/loginStyles";
 import RememberMeCheck from "../../components/RememberMeCheckBox";
 
-
 export default function Login() {
   const navigation = useNavigation();
-  //const RememberMeCheckBox = rememberMeCheckBox();
+  const logoOpacity = useRef(new Animated.Value(0)).current;
+
+  useEffect(() => {
+    Animated.timing(logoOpacity, {
+      toValue: 1,
+      duration: 1200,
+      useNativeDriver: true,
+    }).start();
+  }, []);
 
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Getta Job</Text>
+      {/* Logo com fade-in */}
+      <Animated.Image
+        source={require("../../../assets/icon.png")}
+        style={[styles.logo, { opacity: logoOpacity }]}
+      />
 
       <InputField icon="user" placeholder="Usuário" />
       <InputField icon="lock" placeholder="Senha" secureTextEntry />
       <RememberMeCheck />
-      
 
       <TouchableOpacity
         style={styles.button}
@@ -49,5 +59,3 @@ export default function Login() {
     </View>
   );
 }
-
-
